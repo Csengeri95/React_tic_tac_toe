65e89768cfb64bd1a189f95fca8253045fdb152e
Csengeri95 <csengerilevente95@gmail.com>
2023-05-18 12:14:51 +0200

    Gamebord, Square
    modified to gameboard, added users. Made different styles to the symbols.

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -6,10 +6,42 @@ import Square from "./Square"
 export default function Gameboard() {
 
     const [square, setSquare] = useState(Array(100).fill(""))
+    //const [shape, setShape] = useState("O")
+
+    const players = {
+        first: {
+            name: "Anna",
+            symbol: "O",
+            style: "red"
+        },
+        second: {
+            name: "Levente",
+            symbol: "X",
+            style: "green",
+        }
+    }
+    const [shape, setShape] = useState(players.first)
+
+    console.log(square)
 
     return (
-        <div className="gameboard">
-            {square.map((map,index)=><Square key={index} content={map} index={index} />   )}
+        <div className="gameboard-container">
+            <div className="gameboard">
+                {square.map((map, index) =>
+                    <Square
+                        key={index}
+                        id={index}
+                        players={players}
+                        shape={shape}
+                        setShape={setShape}
+                        square={square}
+                        setSquare={setSquare}
+                    />
+                )}
+
+
+            </div>
+            <p>{shape.name} következik</p>
         </div>
     )
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,70 @@
+import { useEffect, useState } from "react"
 import "../styles/Square.css"
 
 
-export default function Square ({content, index}) {
-    return(
-        <div className="square">
-            <div></div>
+export default function Square({ id, players, shape, setShape, setSquare, square }) {
+
+
+    function handleClick(e) {
+        const reserved = e.target.firstChild.textContent.includes(players.first.symbol) || e.target.firstChild.textContent.includes(players.second.symbol)
+
+
+
+        if (!reserved) {
+
+            if (shape.symbol === players.first.symbol) {
+                e.target.firstChild.textContent = players.first.symbol
+                setShape(players.second)
+                handleColor(players.first.symbol)
+            }
+
+
+            if (shape.symbol === players.second.symbol) {
+                e.target.firstChild.textContent = players.second.symbol
+                setShape(players.first)
+                handleColor(players.second.symbol)
+
+
+            }
+        }
+
+    }
+
+
+
+
+    function handleColor(style) {
+
+        let symbolColor = "";
+
+        if (style === players.first.symbol) {
+            symbolColor = players.first.style;
+        } else if (style === players.second.symbol) {
+            symbolColor = players.second.style;
+        }
+
+        const newSquare = square.map((map, index) => {
+            if (index === id) {
+                return {
+                    style,
+                    symbolColor
+                }
+            }
+            else {
+                return map
+            }
+        })
+
+        setSquare(newSquare)
+
+    }
+
+    console.log(square)
+    return (
+        <div className="square" id={id} onClick={handleClick}  >
+            <div className="cell" style={{ color: square[id].symbolColor }}  >
+
+            </div>
         </div>
     )
 }
